83c7244ec057df265ecd5641994f4f3c30568e40
kranthik2 <kullik2@gmail.com>
2019-06-29 13:10:51 -0500

    added tests

---
+++ src/test/kotlin/com/kulli/ktdemo/controller/SampleControllerTest.kt
@@ -0,0 +1,28 @@
+package com.kulli.ktdemo.controller
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SampleControllerTest {
+
+    private val sampleController:SampleController = SampleController()
+
+    @Before
+    fun setUp() {
+        println("String tests")
+    }
+
+    @Test
+    fun helloWorldGet() {
+        val result = sampleController.helloWorldGet();
+        assertNotNull(result)
+    }
+
+    @Test
+    fun helloWorldPost() {
+       val result = sampleController.helloWorldPost("testValue")
+        assertNotNull(result)
+    }
+}
