5d52dd130fcb80557d9b7ef52bbc6227a2da3984
kranthik2 <kullik2@gmail.com>
2019-06-30 21:45:02 -0500

    fixed gradle docker plugin url

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ import org.jetbrains.kotlin.com.google.common.collect.ImmutableMap
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.1.6.RELEASE"
-	id("io.spring.dependency-management") version "1.0.7.RELEASE"
-	id("com.palantir.docker") version "0.22.1"
-	id ("com.palantir.docker-run") version "0.22.1"
-	kotlin("jvm") version "1.2.71"
-	kotlin("plugin.spring") version "1.2.71"
+    id("org.springframework.boot") version "2.1.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+    id("com.palantir.docker") version "0.22.1"
+    id("com.palantir.docker-run") version "0.22.1"
+    kotlin("jvm") version "1.2.71"
+    kotlin("plugin.spring") version "1.2.71"
 }
 
 group = "com.kulli"
@@ -16,39 +16,38 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
 //gradle docker plugin configuration
 apply(plugin = "com.palantir.docker")
 
 docker {
-	name = "hub.docker.com/mrkulli/ktdemo:".plus(version)
-	tag("url", "hub.docker.com/mrkulli/ktdemo:".plus(version))
-	buildArgs(ImmutableMap.of("name","ktdemo"))
+    name = "mrkulli/ktdemo:".plus(version)
+    uri("mrkulli/ktdemo:".plus(version))
+    tag("name", "ktdemo")
+    buildArgs(ImmutableMap.of("name", "ktdemo"))
     copySpec.from("build").into("build")
-	pull(true)
+    pull(true)
     setDockerfile(file("Dockerfile"))
 }
 
 dockerRun {
-	name = "ktdemo"
-	image = "hub.docker.com/mrkulli/ktdemo:".plus(version)
-	ports("8080:8080")
+    name = "ktdemo"
+    image = "mrkulli/ktdemo:".plus(version)
+    ports("8080:8080")
 }
-
-
